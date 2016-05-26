1dc0e7c7e91015c397c83a1d8e8959e2773ef393
Cathal Killeen <cathalkilleen@gmail.com>
2016-05-26 16:11:56 +0100

    changed stylesheet link

--- head.php
+++ head.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="./text/css" href="style.css">
-<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="./style.css">
+<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet'>
 <title><?php echo $pageTitle ?></title>
 </head>
 
@@ -17,20 +17,17 @@
 	<div id="navbar">
 		<div id="navbar-content">
 			<a href="./index.php"><img src="media/logo.png" class="logo" alt="Mountain Ridge Logo"></a>
-			<ul> 
-			  <li><a href="./tickets-passes.php">Tickets & Passes</a></li> 
-			  <li><a href="./trip-planning.php">Trip Planning</a></li> 
-			  <li><a href="./rentals.php">Rentals</a></li> 
-			  <li><a href="./ski-school.php">Ski School</a></li> 
-			  <li><a href="./events-activities.php">Events & Activities</a></li> 
+			<ul>
+			  <li><a href="./tickets-passes.php">Tickets & Passes</a></li>
+			  <li><a href="./trip-planning.php">Trip Planning</a></li>
+			  <li><a href="./rentals.php">Rentals</a></li>
+			  <li><a href="./ski-school.php">Ski School</a></li>
+			  <li><a href="./events-activities.php">Events & Activities</a></li>
 			  <li><a href="./location.php">Location</a></li>
-			  <li><a href="./contact.php">Contact Us</a></li> 
+			  <li><a href="./contact.php">Contact Us</a></li>
 			</ul>
 		</div>
 	</div>
 
 	<div id="main-content">
 		<div id="body-container">
-
-
-
